cbb68a71accd3642d820f4236340585367953d51
Aochi <shuaochisoft@yahoo.co.jp>
2019-02-01 01:33:59 +0900

    LeetCode

--- LeetCodeExercise/src/algirithm/exercise/Exercise_125.java
+++ LeetCodeExercise/src/algirithm/exercise/Exercise_125.java
@@ -5,13 +5,14 @@ public class Exercise_125 {
 	public boolean isPalindrome(String s) {
 		s = s.toLowerCase();
 		int len = s.length();
+		
 		for(int i = 0 ; i < len ; i++) {
 			char i_char = s.charAt(i);
 			if(i_char >= 'a' && i_char <= 'z') {
 				for(int j = len-1 ; j > i ; j--) {
 					char j_char = s.charAt(j);
 					if(j_char >= 'a' && j_char <= 'z') {
-						return (i_char == j_char)? isPalindrome(s.substring(i, j)):false;
+						return (i_char == j_char)? isPalindrome(s.substring(i+1, j-1)):false;
 					}
 				}
 			}
